f16d4091681536b4ac8de198e6834bd2020e0e3b
Scott Wolchok <swolchok@meta.com>
2025-09-17 09:17:30 -0700

    Add simple MyInt wrapper class with method for benchmarking

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,15 @@ int64_t collatz(int64_t n) {
         return 3 * n + 1;
 }
 
+class MyInt {
+ private:
+  int64_t x = 42;
+ public:
+  int64_t get() const {
+    return x;
+  }
+};
+
 namespace py = pybind11;
 
 PYBIND11_PLUGIN(pybind11_benchmark) {
@@ -14,6 +23,10 @@ PYBIND11_PLUGIN(pybind11_benchmark) {
 
     m.def("collatz", &collatz,
           "Run one iteration of the mapping underlying the Collatz cojecture");
+
+    py::class_<MyInt>(m, "MyInt")
+      .def(py::init<>())
+      .def("get", &MyInt::get);
 
 #ifdef VERSION_INFO
     m.attr("__version__") = py::str(VERSION_INFO);
